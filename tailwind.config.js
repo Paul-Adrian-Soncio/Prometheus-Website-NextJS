bc4b3226b61363693288eae874b071d94cd94dc6
illunaaa-PA <71630552+illunaaa-PA@users.noreply.github.com>
2022-05-11 14:54:49 +0800

    tailwind updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,6 +10,53 @@ module.exports = {
         'visby': ['Visby CF', 'sans-serif'],
         'demi-bold': ['Visby CF Demi-Bold', 'sans-serif'],
       },
+      height: {
+        "20vh": "20vh",
+        "30vh": "30vh",
+        "40vh": "40vh",
+        "60vh": "60vh",
+        "80vh": "80vh",
+        "300px": "300px",
+        "250px": "250px",
+        "200px": "200px",
+        "150px": "150px",
+        "100px": "100px",
+        "50px": "50px",
+        "screen/2": "50vh",
+        "screen/3": "calc(100vh / 3)",
+        "screen/4": "calc(100vh / 4)",
+        "screen/5": "calc(100vh / 5)",
+      },
+      gridTemplaterows: {
+        'layout': 'repeat(2, 1fr)',
+      },
+      gridTemplatecolumns: {
+        'layout': '2fr 1fr',
+      },
+      colors: {
+        'prometheus-gold': '#877562',
+        'prometheus-gray': '#6f6f6f',
+      },
+      spacing: {
+        'full': '100%',
+        '2/3' : '66.666667%',
+      },
+      zIndex: {
+        '-10': '-10',
+      },
+      screens: {
+        'max-xl': {'max': '1279px'},
+        'max-md': {'max': '767px'},
+        'max-sm': {'max': '639px'},
+        '4xl': '2000px',
+        '3xl': '1980px',
+      },
+      maxWidth: {
+        '4k': '2560px',
+      },
+      lineHeight: {
+        '20': '5rem',
+      }
     },
   },
   plugins: [],
